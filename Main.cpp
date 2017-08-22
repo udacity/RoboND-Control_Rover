9e5b5cd464d9cdac0e27526f57b5b0f08b871331
Karim Chamaa <30810934+karimchamaa1@users.noreply.github.com>
2017-08-22 12:42:12 -0700

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include "src/terminal.h"
+#include "src/TCP.h"
+using namespace std;
+
+int main()
+{
+    DisableTerminal();
+    TCPConnect();
+    cout << "\n=> Enter Keyboard Commands to Control the Robot:" << endl;    
+
+    while (1)
+    {
+        switch((int)getchar())
+        {
+            case 'w':
+            cout << "Forward" << endl;
+	    ServerSend("w\n");
+            break;
+
+            case 's':
+            cout << "Backward" << endl;
+	    ServerSend("s\n");
+            break;
+
+            case 'a':
+            cout << "Left" << endl;
+	    ServerSend("a\n");
+            break;
+            
+            case 'd':
+            cout << "Right" << endl;
+	    ServerSend("d\n");
+            break;
+	    
+	    case 'b':
+            cout << "Brake" << endl;
+	    ServerSend("b\n");
+            break;  
+
+	    case 'p':
+            cout << "Pickup" << endl;
+	    ServerSend("p\n");
+            break;  
+
+            default:
+            break;
+	}
+    }
+    return 0;
+}
